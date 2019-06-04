4573bde9a37c94c6dbf46df6ec5ce22a4a8d9ea2
Isc30 <ivansanzcarasa@gmail.com>
2019-06-04 21:45:17 +0200

    code refinements

--- toolchains/emscripten.cmake
+++ toolchains/emscripten.cmake
@@ -4,6 +4,9 @@ if(_is_empty)
     "EMSCRIPTEN environment variable not set. Set EMSCRIPTEN environment variable to point to the emscripten root directory")
 endif()
 
+# Set compiler id
+set(CMAKE_CXX_COMPILER_ID "Emscripten")
+
 # Include native emscripten toolchain
 include("$ENV{EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake")
 
