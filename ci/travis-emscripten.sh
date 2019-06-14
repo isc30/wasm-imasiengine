4e205c14b4a99d4f21c23e5e1fc4b70873b73a0a
Isc30 <ivansanzcarasa@gmail.com>
2019-06-14 20:27:00 +0200

    another way of building emscripten

--- ci/travis-emscripten.sh
+++ ci/travis-emscripten.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-HOMEBREW_NO_AUTO_UPDATE=1 brew install emscripten && export LLVM=/usr/local/opt/emscripten/libexec/llvm/bin && emcc
-
-# Corrade
-git clone --depth 1 git://github.com/mosra/corrade.git
-cd corrade
-
 # Build native corrade-rc
-mkdir corrade-rc && cd corrade-rc
-cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
-    -DWITH_INTERCONNECT=OFF \
-    -DWITH_PLUGINMANAGER=OFF \
-    -DWITH_TESTSUITE=OFF \
-    -DWITH_UTILITY=OFF
+git clone --depth 1 git://github.com/mosra/corrade.git
 
-cmake --build . --target install
+cd corrade
+    mkdir corrade-rc
+    cd corrade-rc
+        cmake .. \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+            -DWITH_INTERCONNECT=OFF \
+            -DWITH_PLUGINMANAGER=OFF \
+            -DWITH_TESTSUITE=OFF \
+            -DWITH_UTILITY=OFF
 
-cd ../..
+        cmake --build . --target install
+    cd ..
+cd ..
+
+wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
+tar -xzf emsdk-portable.tar.gz
+cd emsdk-portable
+    ./emsdk update
+    ./emsdk update-tags
+    ./emsdk install latest
+    ./emsdk activate latest
+    source emsdk_env.sh
+cd ..
 
 # Build using Emscripten toolchain
 cmake . \
     -DCMAKE_CXX_FLAGS="-Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
-    -DCMAKE_TOOLCHAIN_FILE=./toolchains/generic/Emscripten-wasm.cmake \
-    -DEMSCRIPTEN_PREFIX=$(echo /usr/local/Cellar/emscripten/*/libexec)
+    -DCMAKE_TOOLCHAIN_FILE=./toolchains/generic/Emscripten-wasm.cmake
 
 cmake --build .
 
